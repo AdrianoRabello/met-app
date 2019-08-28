fc6f5bbf6d609ff28e8a97dc7c4ccaf9c44a5a3c
adriano.rabello <rabellocbmes@gmail.com>
2019-08-28 16:32:34 -0300

    fazendo o order service

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RadioOption } from 'app/shared/radio/radio-option.model';
 
 @Component({
   selector: 'mt-order',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order.component.css']
 })
 export class OrderComponent implements OnInit {
+
+  paymentOptions:RadioOption[] = [
+
+    {label:"Dinheiro",value: "Mon"},
+    {label:"Cartão de débito",value: "Deb"},
+  ]
 
   constructor() { }
 
